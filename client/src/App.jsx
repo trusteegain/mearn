fa12bfc6b2df1805772d8782ba90f5922dc6ead1
trusteegain <100714808+trusteegain@users.noreply.github.com>
2023-09-07 04:16:03 -0700

    This is the adding of Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import SignIn from "./pages/SignIn"
+import SignUp from "./pages/SignUp"
+import Profile from "./pages/Profile"
 
-function App() {
-   
+const App = () => {
   return (
-     <>
-     <h1 className='text-slate-800 flex-col'>This is a React Full Stack MERN Auth Project</h1>
-     </>
+    <BrowserRouter>
+    
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/sign-in" element={<SignIn />} />
+      <Route path="/sign-up" element={<SignUp />} />
+      <Route path="/profile" element={<Profile />} />
+    </Routes>
+
+    </BrowserRouter>
   )
 }
 
